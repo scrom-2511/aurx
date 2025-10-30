a07ffc0d3db906fb0efd9d43a4cd68d7dd531e22
scrom <formain337@gmail.com>
2025-10-30 15:57:39 +0000

    change file_extension from generic to string

--- backend/src/services/file.rs
+++ backend/src/services/file.rs
@@ -2,10 +2,10 @@ use actix_web::cookie::time::OffsetDateTime;
 
 use crate::types::file_type::FileType;
 
-struct File<T>{
+struct File{
     name: String,
     file_size: u32,
     file_type: FileType,
-    file_extension: T,
+    file_extension: String,
     uploaded_at: OffsetDateTime
 }

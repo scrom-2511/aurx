cef07be4470a5d9cda58c7bb6a229f93ccc97faa
scrom <formain337@gmail.com>
2025-11-05 06:57:19 +0000

    main.rs starting point

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,15 +1,33 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
+use actix_web::{App, HttpServer, web};
+use socket2::{Socket, Domain, Type};
+use std::net::SocketAddr;
+
 mod services;
-mod types;
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    println!("Starting the server...");
+
+    let address: SocketAddr = "127.0.0.1:5000".parse().unwrap();
+
+    let socket = Socket::new(Domain::IPV4, Type::STREAM, None)?;
+    socket.set_reuse_address(true)?;
+    socket.bind(&address.into())?;
+    socket.listen(128)?;
+
+    let listener = socket.into();
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(
+                actix_cors::Cors::default()
+                    .allowed_origin("http://localhost:5173")
+                    .allowed_methods(vec!["GET", "POST"])
+                    .allow_any_header(),
+            )
+            .app_data(web::PayloadConfig::new(10*1024*1024))
+    })
+    .listen(listener)?
+    .run()
+    .await
 }

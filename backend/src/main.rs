892fab96ab93caeeee99466dafbebc13c12f199b
scrom <formain337@gmail.com>
2025-12-29 07:00:52 +0000

    add: ws_handler function to handle websockets

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,22 +1,69 @@
-use crate::services::{
-    database_uploader::DatabaseUploader, file_details::FileDetails, ipfs_uploader::IPFSUploader,
+use crate::{
+    database::schema::file::Model as FileMetadata,
+    services::{
+        database_uploader::DatabaseUploader,
+        file_chunk_manager::{self, FileChunkHashes},
+        ipfs_uploader::IPFSUploader,
+    },
 };
 use actix_web::{
-    App, HttpResponse, HttpServer, Responder, post,
+    App, Error, HttpRequest, HttpResponse, HttpServer, Responder, post, rt,
     web::{self},
 };
+use actix_ws::Message;
+use futures_util::StreamExt;
+use serde::{Deserialize, Serialize};
 use socket2::{Domain, Socket, Type};
-use std::net::SocketAddr;
+use std::{net::SocketAddr, sync::Arc};
+use tokio::sync::Mutex;
 
 mod database;
 mod services;
 
+#[derive(Serialize, Deserialize)]
+struct WebsocketVal {
+    hash: String,
+    latest_chunk: u32,
+    total_chunks: u16,
+    file_id: String,
+}
+
+fn ws_handler(
+    req: HttpRequest,
+    body: web::Payload,
+    file_chunks_manager: web::Data<Arc<Mutex<FileChunkHashes>>>,
+) -> Result<HttpResponse, Error> {
+    let (res, _session, mut stream) = actix_ws::handle(&req, body)?;
+
+    let file_chunks_manager_cloned = file_chunks_manager.clone();
+
+    rt::spawn(async move {
+        while let Some(msg) = stream.next().await {
+            match msg {
+                Ok(Message::Text(text)) => {
+                    if let Ok(data) = serde_json::from_str::<WebsocketVal>(&text) {
+                        file_chunks_manager_cloned.lock().await.add_chunk_hash(data.hash, data.file_id);
+                    }
+                }
+                Ok(Message::Close(_)) => break,
+                Ok(_) => {}
+                Err(e) => {
+                    eprintln!("WebSocket error: {}", e);
+                    break;
+                }
+            }
+        }
+    });
+
+    Ok(res)
+}
+
 #[post("/upload_file")]
 async fn upload_file(
-    file: web::Json<FileDetails>,
+    file: web::Json<FileMetadata>,
     database_uploader: web::Data<DatabaseUploader>,
 ) -> impl Responder {
-    database_uploader.file_upload(file.0, 3).await.unwrap();
+    database_uploader.file_upload(&file.0, 3).await.unwrap();
     HttpResponse::Ok().body("File received")
 }
 
@@ -24,7 +71,7 @@ async fn upload_file(
 async fn main() -> std::io::Result<()> {
     let ipfs_uploader = IPFSUploader::new("https://rpc.filebase.io/api/v0/add");
     let database_uploader = DatabaseUploader::new("postgresql://neondb_owner:npg_wnGc5zvbaCH6@ep-empty-credit-a4il0l00-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require").await.unwrap();
-
+    let file_chunks_manager = Arc::new(Mutex::new(FileChunkHashes::new()));
     let address: SocketAddr = "127.0.0.1:4000".parse().unwrap();
 
     let socket = Socket::new(Domain::IPV4, Type::STREAM, None)?;
@@ -45,6 +92,7 @@ async fn main() -> std::io::Result<()> {
             )
             .app_data(web::PayloadConfig::new(10 * 1024 * 1024))
             .app_data(web::Data::new(ipfs_uploader.clone()))
+            .app_data(web::Data::new(file_chunks_manager.clone()))
             .app_data(web::Data::new(database_uploader.clone()))
     })
     .listen(listener)?
